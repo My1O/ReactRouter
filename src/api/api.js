dd6aa7e585eef2f91e35539da54865a86bade14f
Camilo Castro <castroh24@gmail.com>
2023-06-06 16:59:50 -0600

    axios added, object loaded with map

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const api = axios.create ({
-    baseURL: "http://localhost:5"
+    baseURL: "http://localhost:5000"
 })
 
 export const buscar = async (url, setData) => {
     //
     const respuesta = await api.get(url)
     //l ainformacion obtenida se agrega a setData. si es ./sobre. agrega http://localhost:5000/sobre
+    
     setData(respuesta.data)
 }
